c4196c51724735757edbff498328c164a09dd333
Rohit Gupta <76804979+lucidrohit@users.noreply.github.com>
2023-01-18 00:54:53 +0530

    added remote postgres db

--- config/database.js
+++ config/database.js
@@ -4,10 +4,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "postgres"),
-      user: env("DATABASE_USERNAME", "saeindia"),
+      // host: env("DATABASE_HOST", "127.0.0.1"),
+      // port: env.int("DATABASE_PORT", 5432),
+      // database: env("DATABASE_NAME", "postgres"),
+      // user: env("DATABASE_USERNAME", "saeindia"),
+      // password: env("DATABASE_PASSWORD", "280868"),
+      URL: env("postgres://saeindia_user:KQCICVihL97NlsAQ3P6eduApMht9wwUB@dpg-cf3f379gp3jl0q4791i0-a/saeindia"),
     },
   },
 });
